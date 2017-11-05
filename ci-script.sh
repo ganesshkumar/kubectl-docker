028f8efc24c9bb2ca5e5eb3f7fc6cf7fa2b8e362
Ganessh Kumar <rpganesshkumar@gmail.com>
2017-11-05 22:43:28 +0530

    fix spelling mistake

--- ci-script.sh
+++ ci-script.sh
@@ -13,7 +13,7 @@ else
         sed -i "s/{{VERSION}}/$VERSION/g" Dockerfile && \
         docker build -t ganesshkumar/kubectl:$VERSION . && \
         docker push ganesshkumar/kubectl:$VERSION && \
-        docker tag ganesshkumar/kubectl:$VERSION ganesshkuamr/kubectl:latest && \
+        docker tag ganesshkumar/kubectl:$VERSION ganesshkumar/kubectl:latest && \
         docker push ganesshkumar/kubectl:latest && \
         echo "Created and pushed ganesshkumar/kubectl:$VERSION"
 fi
